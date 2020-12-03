c7e9c6a9e19554b4b7614619737f3bf4907ee8d5
japc <japc.grafico@gmail.com>
2020-12-03 12:13:41 +0100

    Initial commit

---
+++ src/main/java/com/bitcero/springbootwebapp/controller/IndexController.java
@@ -0,0 +1,56 @@
+package com.bitcero.springbootwebapp.controller;
+
+import java.util.Map;
+
+import com.bitcero.springbootwebapp.models.Usuario;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+    @RequestMapping(value={"/index", "/", "/home"}, method = RequestMethod.GET )
+    public String index(Model model) {
+        model.addAttribute("titulo", "Hola Spring Framework");
+        return "index";
+    }
+
+    @GetMapping(value = "/indexModelMap")
+    public String index(ModelMap model) {
+        model.addAttribute("titulo", "Hola Spring Framework with ModelMap");
+        return "index";
+    }
+
+    @GetMapping(value = "/indexMap")
+    public String index(Map<String, Object> map) {
+        map.put("titulo", "Hola Spring Framework with Map");
+        return "index";
+    }
+
+    @GetMapping(value = "/indexModelAndView")
+    public ModelAndView index(ModelAndView mv) {
+        mv.addObject("titulo", "Hola Spring Framework with Model And View");
+        // Se le pasa el nombre de la vista.
+        mv.setViewName("index");
+        return mv;
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Juan");
+        usuario.setApellidos("Pavón");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil de usuario ".concat(usuario.getNombre()));
+
+
+        return "perfil";
+    }
+}
---
+++ src/main/java/com/bitcero/springbootwebapp/models/Usuario.java
@@ -0,0 +1,34 @@
+package com.bitcero.springbootwebapp.models;
+
+public class Usuario {
+    private String nombre;
+    private String apellidos;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
